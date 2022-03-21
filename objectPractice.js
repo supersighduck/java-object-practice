6a216b3d868786694224bfcafcd7183e9f4db11e
supersighduck <99636798+supersighduck@users.noreply.github.com>
2022-03-20 19:13:49 -0700

    Add files via upload

---
+++ objectPractice.js
@@ -0,0 +1,90 @@
+// Create a function that takes two parameters and adds them together -- use the book examples from chapter 3 if you need help making a function
+
+function addFunction(p1,p2) {
+    return p1 + p2;
+}
+
+
+
+
+
+
+// Create a function that takes two parameters and subtracts them
+
+function subtractFunction(p1,p2) {
+    return p2 - p1;
+}
+
+
+
+
+
+
+
+// Create a function that takes two parameters and multiplies them together
+
+function multiplyFunction (p1,p2) {
+    return p1 * p2;
+}
+
+
+
+
+
+
+
+// Create a function that takes two parameters and divides them
+
+function divideFunction (p1,p2) {
+    return p2 / p1;
+}
+
+
+
+
+
+
+
+// Create an array that has 5 elements -- 3 strings and 2 numbers
+
+var array_Practice = ["try", "this", "out", 1, 2];
+
+
+
+
+
+
+
+// Accese the third index from the array that you created from above
+
+array_Practice[2];
+
+
+
+
+
+
+// Create an object with 3 properties using the literal notation syntax (aka syntax == code "writing" style) -- use the book examples from chapter 3 if you need help making an object
+
+var food = {texture:"crispy", color:"yellow", flavor:"savory"};
+
+
+
+
+
+
+
+
+// Create an object with 3 properties using the constructor notation syntax (aka syntax == code "writing" style) -- use the book examples from chapter 3 if you need help making an object
+function Food(texture, color, flavor) {
+    this.texture = texture;
+    this.color = color;
+    this.flavor = flavor;
+}
+
+
+
+
+
+
+
